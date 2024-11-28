886bf2efdc5d6cdbce795cbfc3c0f387f61bb132
mohamaderrr <mohamader14@outlook.fr>
2024-11-28 21:44:24 +0100

    add langtrase integration

--- app/api/email/respond/route.ts
+++ app/api/email/respond/route.ts
@@ -3,7 +3,11 @@ import { ChatGoogleGenerativeAI } from "@langchain/google-genai";
 import { ChatOllama } from "@langchain/ollama";
 import { PromptTemplate } from "@langchain/core/prompts";
 import { StringOutputParser } from "@langchain/core/output_parsers";
+// Must precede any llm module imports
 
+import * as Langtrace from '@langtrace/typescript-sdk'
+
+Langtrace.init({ api_key: '65552fe56cd7374810e5f16c7b3fee1bb7ce9bf3141ed44b896d3f65fac0784c' })
 export const runtime = 'edge';
 
 export async function POST(req: NextRequest) {
